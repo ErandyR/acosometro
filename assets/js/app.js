8802f25971fa7e38ea8fd653576c956a846d8092
Itzel <itzeljimenez.do@gmail.com>
2017-05-14 08:14:56 +0000

    mi version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,3 @@
-var checks=document.getElementsByTagName("input");
-var large=checks.length;
-var count=0;
-var iluminati=document.getElementsByClassName("iluminati");
-for(var i=0; i<large;i++){
-	checks[i].addEventListener("click", counter);
-
-}
-
-
-
 function counter(){
   var cant=0;
   if (document.getElementById('checkbox1').checked)
@@ -16,7 +5,7 @@ function counter(){
     cant++;
   }
   if (document.getElementById('checkbox2').checked)
-  {	
+  { 
     cant++;
   }
   if (document.getElementById('checkbox3').checked)
@@ -52,34 +41,34 @@ function counter(){
   }
   console.log("cant");
   if (cant==1) {
-	iluminati[9].style.visibility="visible";
-	}
-	if (cant==2) {
-	iluminati[8].style.visibility="visible";
-	}
-	if (cant==3) {
-	iluminati[7].style.visibility="visible";
-	}
-	if (cant==4) {
-	iluminati[6].style.visibility="visible";
-	}
-	if (cant==5) {
-	iluminati[5].style.visibility="visible";
-	}
-	if (cant==6) {
-	iluminati[4].style.visibility="visible";
-	}
-	if (cant==7) {
-	iluminati[3].style.visibility="visible";
-	}
-	if (cant==8) {
-	iluminati[2].style.visibility="visible";
-	}
-	if (cant==9) {
-	iluminati[1].style.visibility="visible";
-	}
-	if (cant==10) {
-	iluminati[0].style.visibility="visible";
-	}
+  iluminati[9].style.visibility="visible";
+  }
+  if (cant==2) {
+  iluminati[8].style.visibility="visible";
+  }
+  if (cant==3) {
+  iluminati[7].style.visibility="visible";
+  }
+  if (cant==4) {
+  iluminati[6].style.visibility="visible";
+  }
+  if (cant==5) {
+  iluminati[5].style.visibility="visible";
+  }
+  if (cant==6) {
+  iluminati[4].style.visibility="visible";
+  }
+  if (cant==7) {
+  iluminati[3].style.visibility="visible";
+  }
+  if (cant==8) {
+  iluminati[2].style.visibility="visible";
+  }
+  if (cant==9) {
+  iluminati[1].style.visibility="visible";
+  }
+  if (cant==10) {
+  iluminati[0].style.visibility="visible";
+  }
 };
 
